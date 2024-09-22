54c4f7481944fb038f9f4afc189231f49dfc3a8c
Phabiodnz <Phabiodr@hotmail.com>
2024-09-22 18:13:17 -0300

    Function Operation() concluded and changes to html structure

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-let number1 = 0;
-let number2 = 0;
-let operator = 0;
-
 const add = (num1,num2) => num1 + num2;
 const subtract = (num1,num2) => num1 - num2;
 const multiply = (num1,num2) => num1 * num2;
 const divide = (num1,num2) => num1 / num2;
 
-function operation(operator){
-    if (operator == "+"){
-        return operator = "sum";
-    }else if(operator == "-"){
-        return operator = "subtract";
-    }else if(operator == "x"){
-        return operator = "multiply";
-    }else if(operator == "/"){
-        return operator = "divide";
+function operation(){
+    let getInputValue = document.getElementById("main-input").value;
+    let splitInputString = getInputValue.split("");
+    let convertedNumber1 = parseInt(splitInputString[0]);
+    let convertedNumber2 = parseInt(splitInputString[2]);
+    if(splitInputString[1] == "+"){
+        return add(convertedNumber1,convertedNumber2);
+    }else if(splitInputString[1] == "-"){
+        return subtract(convertedNumber1,convertedNumber2);
+    }else if(splitInputString[1] == "x"){
+        return multiply(convertedNumber1,convertedNumber2);
+    }else if(splitInputString[1] == "/"){
+        return divide(convertedNumber1,convertedNumber2);
     }else{
         return false;
     }
 }
-
-function operate(num1,num2,operator){
-    let x = operation();
-    
-
-
-
-}
-
-
