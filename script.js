ecfa0c794a6abe73cc68f2419a00ef4c89a078a8
Phabiodnz <Phabiodr@hotmail.com>
2024-09-27 19:24:09 -0300

    Changes done to the logic of the calculator, now it has more flexibility on the calculation

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
+let number1 = 0;
+let number2 = 0;
+let operator = 0;
+let clickCounter = 0;
+
 const add = (num1,num2) => num1 + num2;
 const subtract = (num1,num2) => num1 - num2;
 const multiply = (num1,num2) => num1 * num2;
 const divide = (num1,num2) => num1 / num2;
 
-function operation(){
-    let inputElement = document.getElementById("main-input").value;
-    let splitInputString = inputElement.split("");
-    let convertedNumber1 = parseInt(splitInputString[0]);
-    let convertedNumber2 = parseInt(splitInputString[2]);
-    if(splitInputString[1] == "+"){
-        return add(convertedNumber1,convertedNumber2);
-    }else if(splitInputString[1] == "-"){
-        return subtract(convertedNumber1,convertedNumber2);
-    }else if(splitInputString[1] == "x"){
-        return multiply(convertedNumber1,convertedNumber2);
-    }else if(splitInputString[1] == "/"){
-        return divide(convertedNumber1,convertedNumber2);
+
+function getNumber(){ // Get's the number from the input and converts to Integer
+    let inputElement = parseInt(document.getElementById("main-input").value);
+    return inputElement;
+}
+
+function numberChosen(){ // Chooses wich number it will get based on the number of times the button was clicked
+    if(clickCounter == 0){
+        clickCounter++
+        number1 = getNumber();
+    }else{
+        clickCounter = 0;
+        number2 = getNumber();
+    }
+}
+
+function getOperation(){ // Get's the operation based on wich button you pressed before
+    let operationAtTheMoment = operator;
+    return operationAtTheMoment;
+}
+function setOperation(operationWanted){ // Set's the operation based on wich button you pressed and reset's the main-input
+    operator = operationWanted;
+    numberChosen();
+    let inputElement = document.getElementById("main-input");
+    inputElement.value = "";
+}
+
+function operation(){ // Operates based on the operator that was chosen
+    let operationWanted = operator;
+    if (operationWanted == '+'){
+        return add(number1,number2);
+    }else if (operationWanted == '-'){
+        return subtract(number1,number2);
+    }else if (operationWanted == 'x'){
+        return multiply(number1,number2);
+    }else if (operationWanted == '/'){
+        return divide(number1,number2);
     }else{
         return false;
     }
@@ -23,12 +52,7 @@ function operation(){
 
 function typeOnInput(typedNumber) {
     let inputElement = document.getElementById("main-input");
-    let splitInputString = inputElement.value.split("");
-    if (splitInputString.length < 3){
-        inputElement.value = inputElement.value + typedNumber;
-    }else{
-        alert("The operation consists on 2 numbers and an operator between them, use the CLEAR BUTTON and try again");
-    }
+    inputElement.value = inputElement.value + typedNumber;
 }
 
 function clearInput(){
@@ -37,6 +61,7 @@ function clearInput(){
 }
 
 function showResult(){
+    numberChosen();
     let result = document.getElementById("result");
     result.textContent = ("The Result is: " + operation());
 }
